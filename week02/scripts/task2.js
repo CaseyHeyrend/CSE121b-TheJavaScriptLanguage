d6d72f24ad82ad20a429b526c5327beb19a72bff
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-09-24 20:40:02 -0600

    changes to week 2

--- week02/scripts/task2.js
+++ week02/scripts/task2.js
@@ -1,36 +1,41 @@
 /* Lesson 2 */
-
+document.getElementById('elementId');
 /* VARIABLES */
 
 // Step 1: declare and instantiate a variable to hold your name
-
+let myName = "Casey Heyrend";
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
-
+const modifyName = document.querySelector("#name");
+modifyName.textContent = myName;
 
 // Step 3: declare and instantiate a variable to hold the current year
-
+let currentYear = "2022";
 
 // Step 4: place the value of the current year variable into the HTML file
-
+const modifyYear = document.querySelector("#year");
+modifyYear.textContent = currentYear;
 
 // Step 5: declare and instantiate a variable to hold the name of your picture
-
+let myImage = "./images/CaseyHeyrend.jpeg";
+let myImageDes = "An image of myself."
 
 // Step 6: copy your image into the "images" folder
 
 // Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
-
+const modifyImage = document.querySelector("img");
+modifyImage.setAttribute("src", myImage), ("alt", myImageDes);
 
 
 
 /* ARRAYS */
 
 // Step 1: declare and instantiate an array variable to hold your favorite foods
-
+var food = ["Pizza", "Noodles", "Cookies"]
+const modifyfood =document.querySelector("#food");
 
 // Step 2: place the values of the favorite foods variable into the HTML file
-
+modifyfood.textContent= (food[0] + ", " + food[1] + ", and " + food[2]);
 
 // Step 3: declare and instantiate a variable to hold another favorite food
 
