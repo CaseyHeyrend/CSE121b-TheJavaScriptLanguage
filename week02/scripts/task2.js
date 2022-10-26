b6d14662214028923fb486fa4d4df4d655337678
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2022-10-26 00:50:20 -0600

    meh I drop this class

--- week02/scripts/task2.js
+++ week02/scripts/task2.js
@@ -1,49 +1,51 @@
 /* Lesson 2 */
-document.getElementById('elementId');
+
 /* VARIABLES */
 
 // Step 1: declare and instantiate a variable to hold your name
-let myName = "Casey Heyrend";
+const myName = "Casey Heyrend"
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
-const modifyName = document.querySelector("#name");
-modifyName.textContent = myName;
+document.querySelector("#name").innerHTML = myName
 
 // Step 3: declare and instantiate a variable to hold the current year
-let currentYear = "2022";
+const year = 2022
 
 // Step 4: place the value of the current year variable into the HTML file
-const modifyYear = document.querySelector("#year");
-modifyYear.textContent = currentYear;
+document.querySelector("#year").innerHTML = year
 
 // Step 5: declare and instantiate a variable to hold the name of your picture
-let myImage = "./images/CaseyHeyrend.jpeg";
-let myImageDes = "An image of myself."
+const picture = "./images/CaseyHeyrend.jpeg";
 
 // Step 6: copy your image into the "images" folder
 
 // Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
-const modifyImage = document.querySelector("img");
-modifyImage.setAttribute("src", myImage), ("alt", myImageDes);
+document.querySelector("img").setAttribute("src", picture)
 
 
 
 /* ARRAYS */
 
 // Step 1: declare and instantiate an array variable to hold your favorite foods
-var food = ["Pizza", "Noodles", "Cookies"]
+let basicFoods = ["Pizza", "Noodles", "Cookies", "Pocky", "Water", "Fruits"]
 
 // Step 2: place the values of the favorite foods variable into the HTML file
-const modifyfood =document.querySelector("#food");
+document.querySelector("#food").innerHTML = basicFoods
 
 // Step 3: declare and instantiate a variable to hold another favorite food
-modifyfood.textContent= (food[0] + ", " + food[1] + ", and " + food[2]);
+const anotherFood = "people"
 // Step 4: add the variable holding another favorite food to the favorite food array
+basicFoods.push(anotherFood)
 // Step 5: repeat Step 2
+document.querySelector("#food").innerHTML = basicFoods
 // Step 6: remove the first element in the favorite foods array
+basicFoods.shift()
 // Step 7: repeat Step 2
+document.querySelector("#food").innerHTML = basicFoods
 // Step 8: remove the last element in the favorite foods array
+basicFoods.pop()
 // Step 7: repeat Step 2
+document.querySelector("#food").innerHTML = basicFoods
 
 
 
--- week04/scripts/task4.js
+++ week04/scripts/task4.js
@@ -1,53 +1,59 @@
 /* Lesson 4 */
-document.getElementById('elementId');
+
 /* DATA */
 
 // Step 1: Declare a new variable to hold information about yourself
-let myName = "Casey Heyrend";
+let Casey = {}
 // Step 2: Inside of the object, add a property named name with a value of your name as a string
-const modifyName = document.querySelector("#name");
-modifyName.textContent = myName;
+casey.name = "Casey Heyrend"
 // Step 3: Add another property named photo with a value of the image path and name (used in Task 2) as a string
-let myImage = "./images/CaseyHeyrend.jpeg";
-let myImageDes = "An image of myself."
-const modifyImage = document.querySelector("img");
-modifyImage.setAttribute("src", myImage), ("alt", myImageDes);
+casey.photo = "/images/CaseyHeyrend.jpeg"
 // Step 4: Add another property named favoriteFoods with a value of an array of your favorite foods as strings ( hint: [] )
-var food = ["Pizza", "Noodles", "Cookies"]
-const modifyfood = document.querySelector("#favorite-foods");
-modifyfood.textContent= (food[0] + ", " + food[1] + ", and " + food[2]);
+casey.favoriteFoods =["Pizza", "Noodles", "Cookies"]
 // Step 5: Add another property named hobbies with a value of an array of your hobbies as strings
-var hobbies = ["Playing the Games", "The Internet", "Daydream"]
-const modifyhobbies = document.querySelector("#hobbies");
-modifyhobbies.textContent= (hobbies[0] + ", " + hobbies[1] + ", and " + hobbies[2]);
+casey.hobbies =  ["Playing the Games", "The Internet", "Daydream"]
 // Step 6: Add another property named placesLived with a value of an empty array
+casey.placesLived = []
 var places = ["Soda Springs", "Montpelier", "Rigby"]
 
 // Step 7: Inside of the empty array above, add a new object with two properties: place and length and values of an empty string
-
+casey.placesLived.push({place: '', length: ''})
 // Step 8: For each property, add appropriate values as strings
-const modifyplaces = document.querySelector("#places-lived");
+casey.placesLived[0].place = 'Soda Springs, Idaho'
+casey.placesLived[0].length = 25
 // Step 9: Add additional objects with the same properties for each place you've lived
-modifyplaces.textContent= (places[0] + ", " + places[1] + ", and " + places[2]);
+casey.placesLived.push({place: '', length: ''})
+casey.placesLived.push({place: '', length: ''})
+casey.placesLived[1].place = 'Montpelier, Idaho'
+casey.placesLived[1].length = 2
+casey.placesLived[2].place = 'Rigby, Idaho'
+casey.placesLived[2].length = 2
 
 /* OUTPUT */
 
 // Step 1: Assign the value of the name property (of the object declared above) to the HTML <span> element with an ID of name
-
+document.querySelector('#name').innerHTML = casey.name
 // Step 2: Assign the value of the photo property as the src attribute of the HTML <img> element with an ID of photo
-
+document.querySelector('#photo').src = casey.photo
 // Step 3: Assign the value of the name property as the alt attribute of the HTML <img> element with an ID of photo
-
+document.querySelector('#photo').alt = casey.name
 // Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
-
+casey.favoriteFoods.forEach(el => {
+    document.querySelector('#favorite-foods').innerHTML += (`<li>${el}</li>`)
+})
 // Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
 
 // Step 6: Repeat Step 4 for each hobby in the hobbies property
-
+casey.hobbies.forEach(el => {
+    document.querySelector('#hobbies').innerHTML += (`<li>${el}</li>`)
+})
 // Step 7: Repeat Step 5 using the HTML <ul> element with an ID of hobbies
 
 // Step 8: For each object in the <em>placesLived</em> property:
 // - Create an HTML <dt> element and put its place property in the <dt> element
 // - Create an HTML <dd> element and put its length property in the <dd> element
-
+casey.placesLived.forEach(el => {
+    document.querySelector('#places-lived').innerHTML += (`<dt>${el.place}</dt>`)
+    document.querySelector('#places-lived').innerHTML += (`<dd>${el.length} years</dd>`)
+})
 // Step 9: Append the HTML <dt> and <dd> elements created above to the HTML <dl> element with an ID of places-lived
