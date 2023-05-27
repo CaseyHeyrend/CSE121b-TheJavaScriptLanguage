b165b2866a335264bd1e9a2f913449bc72cb0765
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2023-05-26 23:12:42 -0600

    ummmmmmm

--- week06_project/scripts/project.js
+++ week06_project/scripts/project.js
@@ -1,25 +1,40 @@
-window.addEventListener("DOMContentLoaded", () => {
-	document.getElementById("calculate").addEventListener("click", () => {
-		const amount = parseFloat(document.getElementById("amount").value);
-        
-        if (isNaN(amount)) return;
-
-		const tipEl = document.getElementById("tip");
-		const tip = Number.parseInt(tipEl.options[tipEl.selectedIndex].value).toFixed(
-			2
-		);
-		const people = Number.parseInt(document.getElementById("people").value);
-
-		const tipInDollars = amount * (tip / 100);
-		const subTotal = parseFloat(amount + tipInDollars);
-		const total = subTotal / people;
-
-		document.querySelector(".info__tip").innerHTML = `⭐️Tip: $${parseFloat(
-			tipInDollars
-		).toFixed(2)}`;
-		document.querySelector(".info__total").innerHTML = `💰 Total: $${total} ${
-			people > 1 ? "each" : ""
-		}`;
-		document.querySelector(".info").style = "display: block";
-	});
-});
+//Calculate Tip
+function calculateTip() {
+	var billAmt = document.getElementById("billamt").value;
+	var serviceQual = document.getElementById("serviceQual").value;
+	var numOfPeople = document.getElementById("peopleamt").value;
+  
+	//validate input
+	if (billAmt === "" || serviceQual == 0) {
+	  alert("Please enter values");
+	  return;
+	}
+	//Check to see if this input is empty or less than or equal to 1
+	if (numOfPeople === "" || numOfPeople <= 1) {
+	  numOfPeople = 1;
+	  document.getElementById("each").style.display = "none";
+	} else {
+	  document.getElementById("each").style.display = "block";
+	}
+  
+	//Calculate tip
+	var total = (billAmt * serviceQual) / numOfPeople;
+	//round to two decimal places
+	total = Math.round(total * 100) / 100;
+	//next line allows us to always have two digits after decimal point
+	total = total.toFixed(2);
+	//Display the tip
+	document.getElementById("totalTip").style.display = "block";
+	document.getElementById("tip").innerHTML = total;
+  
+  }
+  
+  //Hide the tip amount on load
+  document.getElementById("totalTip").style.display = "none";
+  document.getElementById("each").style.display = "none";
+  
+  //click to call function
+  document.getElementById("calculate").onclick = function() {
+	calculateTip();
+  
+  };
